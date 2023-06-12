3783dcf17e7ed390823da4f7e346fcba8b709b95
Mohamed Abou El Khir <mohamedabouelkhir34@gmail.com>
2023-06-12 05:07:54 +0100

    third commit

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -156,6 +156,7 @@ const AppHeader = () => {
 						Projects
 					</Link>
 					<Link
+						href="/extracurricular_activities"
 						to="/extracurricular_activities"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 						aria-label="About Me"
