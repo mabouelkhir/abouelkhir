930972a183a482f06c2715d0cd7aa8a87d2f8901
Mohamed Abou El Khir <mohamedabouelkhir34@gmail.com>
2023-06-13 04:16:02 +0100

    third commit

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -140,35 +140,35 @@ const AppHeader = () => {
 
 				{/* Header links large screen */}
 				<div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
-					<Link
-						to="/projects"
+					<a
+						href="/projects"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
 						aria-label="Projects"
 					>
 						Projects
-					</Link>
-					<Link
+					</a>
+					<a
 						href="/extracurricular_activities"
 						to="/extracurricular_activities"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 						aria-label="About Me"
 					>
 						Extracurricular Activities
-					</Link>
-					<Link
-						to="/about"
+					</a>
+					<a
+						href="/about"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
 						aria-label="About Me"
 					>
 						About Me
-					</Link>
-					<Link
-						to="/contact"
+					</a>
+					<a
+						href="/contact"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
 						aria-label="Contact"
 					>
 						Contact
-					</Link>
+					</a>
 				</div>
 
 				{/* Header right section buttons */}
